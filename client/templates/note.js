8896bf610e41029116ece2d22a7790be8932df18
meiroo <meiroo@outlook.com>
2014-11-09 23:42:18 +0800

    confirm delete

--- client/templates/note.js
+++ client/templates/note.js
@@ -11,6 +11,22 @@ Template.note.events({
       alert("只能删除自己的Note!!!");
      return false;
    }
+   $('#confirmDelete').modal('show');
+  //Notes.remove( this._id );
+},
+
+"click #confirmDelete .confirmdelete": function (event,template) {
+   var usr =  Meteor.user();
+   if(!usr){
+     alert("登录后才可以删除Note !!!");
+     return false;
+   }
+   var author = usr.emails[0].address;
+   if(author !== this.author && author !== "meiroo@outlook.com"){
+      alert("只能删除自己的Note!!!");
+     return false;
+   }
+   //$('#confirmDelete').modal('show');
   Notes.remove( this._id );
 },
 
